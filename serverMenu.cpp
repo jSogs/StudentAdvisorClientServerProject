06927f011ef142fa84ead4bba3eeb9e5e771949f
Julian Asogwa <julianasogwa96@gmail.com>
2024-12-06 14:30:10 -0600

    Modified new feature

--- serverMenu.cpp
+++ serverMenu.cpp
@@ -40,7 +40,6 @@ again:
 			handleRegisterClasses(parts, sockfd);
 		else if (requestType == "BILL")
 			handlePayBill(parts, sockfd);
-		else if (requestType == "");
 		else if (requestType == "VIEW")
 			handleVieworDropClasses(parts, sockfd);
 		memset(req, 0, MAXLINE);
